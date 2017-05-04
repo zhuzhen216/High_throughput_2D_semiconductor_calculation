501cc727fd0cdfc35c730d1a9ea56faa2b6e886f
zhuzhen216 <zhnzhu@gmail.com>
2017-05-03 21:46:56 -0700

    Update POSCAR_generation.py

--- POSCAR_generation.py
+++ POSCAR_generation.py
@@ -7,6 +7,7 @@ import os
 import shutil
 import numpy
 import matplotlib.pyplot as plt
+import math
 #get_ipython().magic('matplotlib inline')
 
 
@@ -27,8 +28,8 @@ class element:
 class structure_and_lattice:
     compare_radius = 98
     # black phosphorus, blue phosphorus, SiS structure
-    frac=[[(0.75,0.5997,0.2229),(0.25,0.0997,0.0718),(0.25,0.014384,0.2094),(0.75,0.5144,0.0854)],[(0.0,0.0,0.0113),(0.333,0.333,-0.05132)]]
-    lattice = [[(3.348,0.,0.),(0.,4.623,0.),(0.,0.,20.)],[(2.881076913,-1.662861501,0.0),(2.881077039,1.662861720,0.0),(0.0,0.0,20.)]]
+    frac=[[(0.75,0.5997,0.2229),(0.25,0.0997,0.0718),(0.25,0.014384,0.2094),(0.75,0.5144,0.0854)],[(0.0,0.0,0.0113),(0.333,0.333,-0.05132)],[(0.0,0.25,0.5785),(0.5,0.75,0.42152),(0.,0.25,0.41),(0.5,0.75,0.59)]]
+    lattice = [[(3.348,0.,0.),(0.,4.623,0.),(0.,0.,20.)],[(2.881076913,-1.662861501,0.0),(2.881077039,1.662861720,0.0),(0.0,0.0,20.)],[(3.6,0,0),(0,3.6,0),(0,0,20)]
     lattice_sites = [4,2]
     phase = [0,1]
 
@@ -85,10 +86,10 @@ def scale(compound):
     scale_factor = 1.
     if len(seperate)==1:
         scale_factor = element.atomic_radius[seperate[0]]/structure_and_lattice.compare_radius
-        return scale_factor
+        return math.sqrt(scale_factor)
     else:
         scale_factor = sum(element.atomic_radius[chars] for chars in seperate)/structure_and_lattice.compare_radius/2
-        return scale_factor
+        return math.sqrt(scale_factor)
 #scale('P')
 #tuple(map(lambda i:i*2,(1,2)))
 
@@ -99,7 +100,7 @@ def scale(compound):
 # phase = 0, 1, 2
 def create_lattice(compound,phase):
     return [tuple(map(lambda x: x*scale(compound),structure_and_lattice.lattice[phase][i])) for i in range(len(structure_and_lattice.lattice[phase]))]
-create_lattice('GeSe',0)
+#create_lattice('GeSe',0)
 
 
 # In[212]:
@@ -135,7 +136,9 @@ def write_POSCAR_group(group):
 
 # In[214]:
 
-#write_POSCAR_group(elem_V)
+write_POSCAR_group(elem_V)
+write_POSCAR_troup(compound_III_VII)
+write_POSCAR_troup(compound_IV_VI)
 #compound_III_VII
 
 
@@ -144,10 +147,10 @@ def write_POSCAR_group(group):
 ## set up folders and cp the POSCAR file to correct folder
 # os.path module
 # the idea is:
-# - you will initialize a new path: new_folder=os.path.join(current_path,folder_name)
+# - you will initialize a new path: new_folder=os.path.join(folder_path,folder_name)
 # - then use os.makedirs(new_folder) to create the folder
-def create_folder(current_path,folder_name):
-    new_folder=os.path.join(current_path,folder_name)
+def create_folder(folder_path,folder_name):
+    new_folder=os.path.join(folder_path,folder_name)
     if not os.path.exists(new_folder):
         os.makedirs(new_folder)
 
@@ -160,11 +163,11 @@ def create_folder(current_path,folder_name):
 # In[217]:
 
 # create a lot of folders:
-current_path = '/Users/zhenzhu/Project/calculcation/IV-VI/lots_of_struc'
+folder_path = '/Users/zhenzhu/Project/calculcation/IV-VI/lots_of_struc'
 for folder_name in compound_all:
     for phase_name in compound_phase[folder_name]:
         combined_name = os.path.join(folder_name,phase_name)
-        create_folder(current_path,combined_name)   
+        create_folder(folder_path,combined_name)   
 
 
 # In[218]:
@@ -173,7 +176,7 @@ for folder_name in compound_all:
 # os.listdir can show all the files or folders in current dir
 # and store the name in a returned list
 #
-file_dir = '/Users/zhenzhu/Project/calculcation/NaMnO2/HSE/transition_level/Al_Na/POSCAR'
+file_dir = os.getcwd()
 folder_dir = '/Users/zhenzhu/Project/calculcation/IV-VI/lots_of_struc'
 files=os.listdir(file_dir)
 for file_name in files:
@@ -184,10 +187,7 @@ for file_name in files:
     #if len(file_name_list)==1:
     #    continue
     shutil.copy2(os.path.join(file_dir,file_name),os.path.join(folder_dir,file_name_list[1],'_'.join(file_name_list[1:]),'POSCAR'))
-
-
 # In[219]:
-
 #
 # create correct POTCAR files
 #
@@ -226,9 +226,3 @@ for compound in compound_all:
     for phase_name in compound_phase[compound]:
         outfile_path = os.path.join(folder_dir,compound,phase_name,'POTCAR')
         combine_file(filenames,outfile_path)
-
-
-# In[ ]:
-
-
-
